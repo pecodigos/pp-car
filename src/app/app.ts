2473a6652847b48d914a28d99b0db4180beb665e
pecodigos <pedrenriquecunha@gmail.com>
2025-09-05 20:22:56 -0300

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
